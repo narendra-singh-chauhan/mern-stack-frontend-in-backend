25ef264edf307a5280fb8511ccacaddbfd1f971e
Narendra Singh <narendrasinghks2019@gmail.com>
2023-03-19 19:10:09 +0530

    Test

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/dist'));
 
     app.get('/', (req, res) => {
-        res.sendFile(express.static('client/dist/index.html'), function(error){
+        res.sendFile(express.static('client/dist', 'index.html'), function(error){
             if(error){
                 res.status(400).json({err : error});
             }
